b27a58db0101dfe84450bc1c0c5e26a7b44b542b
PavelKriz <45934188+PavelKriz@users.noreply.github.com>
2022-01-06 09:16:31 +0100

    working opencv graphing

--- heart_rate_plot.py
+++ heart_rate_plot.py
@@ -1,49 +1,47 @@
-import matplotlib
-matplotlib.use('tkagg')
-import matplotlib.pyplot as plt
-
-#import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib.animation import FuncAnimation
-
 import collections
+import cv2
+import numpy as np
+import random
+
+# Plot values in opencv program
+class Plotter:
+    def __init__(self, plot_width, plot_height, plot_step_size):
+        self.step_size = plot_step_size
+        self.plotSize = int(plot_width / plot_step_size)
+        self.width = plot_width
+        self.height = plot_height
+        self.color = (255, 0 ,0)
+        self.val = collections.deque(np.zeros(5, dtype=int))
+        self.plot = np.ones((self.height, self.width, 3))*255
+        self.offset = 50 # for legend, etc...
+
+    # Update new values in plot
+    def plot_graph(self, val, label = "plot"):
+        self.val.append(int(val))
+        while len(self.val) > self.plotSize:
+            self.val.popleft()
+            self.plot = np.ones(self.plot.shape, self.plot.dtype)*255
+
+        self.show_plot(label)
+
+    # Show plot using opencv imshow
+    def show_plot(self, label):
+        cv2.line(self.plot, (0, int(self.height/2) ), (self.width, int(self.height/2)), (0,255,0), 1)
+        for i in range(len(self.val)-1):
+            cv2.line(self.plot, (i * self.step_size, int(self.height/2 - self.val[i])), ((i+1) * self.step_size , int(self.height/2 - self.val[i+1])), self.color, 1)
+
+        cv2.imshow(label, self.plot)
+        cv2.waitKey(30)
 
 
-class DataGiver:
-    def cpu(self):
-        return 40
+plotter = Plotter(1200, 300, 10)
+# Create dummy values using for loop
+while True:
+    maxVal = random.randint(50,150)
+    for v in range(0,maxVal,10):
+    # call ‘plot’ method for realtime plot
+        plotter.plot_graph(v)
 
-    def ram(self):
-        return 40
-
-# OPENCV NOT WORKING WITH MATPLOTLIB WINDOW
-class data_plot():
-    def __init__(self):
-        # start collections with zeros
-        self.cpu = collections.deque(np.zeros(10))
-        self.dg = DataGiver()
-
-    def animate_func(self, i):
-        # get data
-        self.cpu.popleft()
-        self.cpu.append(self.dg.cpu())
-
-        # clear axis
-
-        self.ax.cla()
-        self.ax.plot(self.cpu)
-        self.ax.scatter(len(self.cpu) - 1, self.cpu[-1])
-        self.ax.text(len(self.cpu) - 1, self.cpu[-1] + 2, "{}%".format(self.cpu[-1]))
-        self.ax.set_ylim(0, 100)
-
-    def start(self):
-        # define and adjust figure
-        self.fig = plt.figure(figsize=(12, 6), facecolor='#DEDEDE')
-        self.ax = plt.subplot()
-        self.ax.set_facecolor('#DEDEDE')
-
-        # animate
-        self.ani = FuncAnimation(self.fig, self.animate_func, interval=1000)
-        print("beforeplot")
-        plt.show(block = False)
-        print("afterplot")
+    for v in range(maxVal, 0 , -10):
+    # call ‘plot’ method for realtime plot
+        plotter.plot_graph(v)
